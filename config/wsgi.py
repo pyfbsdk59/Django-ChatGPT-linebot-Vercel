3b6c1cc3e53cc346e8cd53556990b9e057706574
pyfbsdk59 <pyfbsdk59@gmail.com>
2022-12-24 21:36:51 +0800

    7th commit

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+app = application
