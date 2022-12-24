6a9460c36354f71435f947f46950b9266e3599e1
pyfbsdk59 <pyfbsdk59@gmail.com>
2022-12-25 07:46:21 +0800

    11th commit

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,21 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
